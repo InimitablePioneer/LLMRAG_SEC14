6d8e13a36dba119ee11c2f9ddeaf6d74753212cd
InimitablePioneer <seungwonpioneer@gmail.com>
2024-10-20 17:45:11 +0900

    OCT20 at cafe via test OLD MAC

--- src/main/java/hellospring/loginpage/LoginpageApplication.java
+++ src/main/java/hellospring/loginpage/LoginpageApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoginpageApplication {
+public class LoginpageApplication {//레거시 맥북 커밋 테스트 주석
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginpageApplication.class, args);
