77ecb58acb4265aeed1bc68e3b935028d5365997
InimitablePioneer <145961960+InimitablePioneer@users.noreply.github.com>
2024-10-24 22:58:37 +0900

    OCT24 at home via MAC

--- src/main/java/hellospring/loginpage/web/login/LoginService.java
+++ src/main/java/hellospring/loginpage/web/login/LoginService.java
@@ -1,4 +1,15 @@
 package hellospring.loginpage.web.login;
 
+import hellospring.loginpage.domain.user.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
+
+    private final UserRepository userRepository;
+
+
+    public LoginService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
