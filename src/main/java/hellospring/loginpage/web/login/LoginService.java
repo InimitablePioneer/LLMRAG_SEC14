a487d5772e06fe80e9a77271e521f276292d868c
InimitablePioneer <seungwonpioneer@gmail.com>
2024-10-25 14:56:11 +0900

    OCT25 at work via WIN

--- src/main/java/hellospring/loginpage/web/login/LoginService.java
+++ src/main/java/hellospring/loginpage/web/login/LoginService.java
@@ -1,7 +1,10 @@
 package hellospring.loginpage.web.login;
 
+import hellospring.loginpage.domain.user.User;
 import hellospring.loginpage.domain.user.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class LoginService {
@@ -12,4 +15,17 @@ public class LoginService {
     public LoginService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+    public User login(String loginId, String password) {
+        Optional<User> optionalMember = userRepository.findByLoginId(loginId);
+        if (optionalMember.isPresent()) {
+            User user = optionalMember.get();
+            if (user.getPassword().equals(password)) {
+                return user; // 로그인 성공
+            }
+        }
+        return null; // 로그인 실패
+    }
+
+
 }
