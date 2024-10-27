da079b77b8762a4b7bfb3344468c7e334663918b
InimitablePioneer <seungwonpioneer@gmail.com>
2024-10-27 15:56:19 +0900

    OCT27 at work via MAC

--- src/main/java/hellospring/loginpage/web/session/SessionManager.java
+++ src/main/java/hellospring/loginpage/web/session/SessionManager.java
@@ -1,4 +1,31 @@
 package hellospring.loginpage.web.session;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class SessionManager {
+
+    public static final String SESSION_USER = "user";
+
+    private Map<String, Object> sessionMap = new ConcurrentHashMap<>();
+
+    private Cookie findCookie(HttpServletRequest request, String cookieName) {
+        if (request.getCookies() == null) {
+            return null;
+        }
+        for (Cookie cookie : request.getCookies()) {
+            if (cookie.getName().equals(cookieName)) {
+                return cookie;
+            }
+        }
+        return null;
+    }
+
+
+
+
+
 }
